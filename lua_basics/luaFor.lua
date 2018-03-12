917aa6e3ad077c9fa270657a49988cedf303c6ba
Glenn Lopez <glennlopez@gmail.com>
2018-03-12 14:36:42 -0500

    update

--- lua_basics/luaFor.lua
+++ lua_basics/luaFor.lua
@@ -2,3 +2,9 @@
     For loops in Lua:
     https://www.youtube.com/watch?v=iMacxZQMPXs&t=1320s
 ]]
+
+-- initiate var, condition to loop, increment by
+for i = 1, 10, 1 do
+    io.write(i)
+end
+io.write("\n")
---
+++ lua_basics/luaTables.lua
@@ -0,0 +1,5 @@
+--[[
+    Tables in Lua:
+    https://www.youtube.com/watch?v=iMacxZQMPXs&t=1421s
+]]
+
