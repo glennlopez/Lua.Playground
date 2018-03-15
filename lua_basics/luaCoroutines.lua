3afc2606702da838c515744027cdc2e65ee44ff0
Glenn Lopez <glennlopez@gmail.com>
2018-03-15 15:39:25 -0500

    finished

--- lua_basics/luaCoroutines.lua
+++ lua_basics/luaCoroutines.lua
@@ -1,4 +1,25 @@
 --[[
-    Lua Co-Routines
+    Lua Co-Routines (threadding): allow us to execute several tasks at once
     https://www.youtube.com/watch?v=iMacxZQMPXs&t=2206s
+    http://lua-users.org/wiki/CoroutinesTutorial
 ]]
+
+-- create a coroutine 
+co = coroutine.create( 
+    function()
+        -- print out the loop increments & coroutine status
+        for i = 1, 10, 1 do
+            print(i)
+            print(coroutine.status( co )) -- STATUS
+
+            -- stop coroutine
+            if i == 5 then
+                coroutine.yield() 
+            end
+        end 
+    end
+)
+
+print(coroutine.status( co )) -- STATUS
+coroutine.resume( co )
+print(coroutine.status( co )) -- STATUS
