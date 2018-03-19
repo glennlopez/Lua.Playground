f2f3e209c7311e6c29ed9f307f6e9a7211671620
Glenn Lopez <glennlopez@gmail.com>
2018-03-19 11:25:01 -0500

    finished fileio lesson

--- lua_basics/luaFileIO.lua
+++ lua_basics/luaFileIO.lua
@@ -4,18 +4,53 @@
     https://www.tutorialspoint.com/lua/lua_file_io.htm
 
     Note:
-        - Folders have to exist; Lua will not create them for you
+        - Folders have to exist; Lua will not create them for you automatically 
+
+    Sequence:
+        1) Create a file pointer
+        2) Set file pointer direction (input or output)
+        3) Read, Write, or Append to the file pointer
+        4) Close the file pointer
 ]]
 
 
--- creates a file pointer: test_file
-test_file = io.open("luaFileIO/append.lua", "a")
+-- creates a file pointer: append_file, write_file
+append_file = io.open("luaFileIO/append.txt", "a+")
+write_file = io.open("luaFileIO/write.txt", "w+")
 
--- set output to file pointer
-io.output(test_file)
-io.write(" -- End of the test.lua file\n")
+--[[
+    APPENDING TEXT TO FILES
+]]
+-- set the file pointer direction to output
+io.output(append_file)
+
+-- write to the file pointer
+io.write("-- End of the append.txt file\n")
 
 -- another method of doing the same as above
-test_file:write("-- appended using pointer:write(\"...\")\n")
-io.close(test_file)
+append_file:write("-- appended using pointer:write(\"...\")\n")
 
+-- set the file pointer
+append_file:seek("set", 11)
+
+-- read everything in the file pointer
+print(append_file:read("*a")) -- will not work if file mode is not + (a+, w+, r+)
+
+-- close file pointer
+append_file:close() -- io.close(append_file) --> also works
+
+
+--[[
+    WRITING TEXT TO FILES
+]]
+-- manipulate this file pointer
+io.output(write_file)
+
+write_file:write("First\n")
+write_file:write("Second\n")
+write_file:write("Last\n")
+
+-- set the file pointer cursor to the start to read
+write_file:seek("set", 0)
+print(write_file:read("*a"))
+write_file:close()
